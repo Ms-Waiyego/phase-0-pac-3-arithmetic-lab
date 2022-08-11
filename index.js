dbed8ff40374254d79ead6a9b267b252c742640d
Brenda <waiyegobrenda99@gmail.com>
2022-08-11 18:58:36 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,48 @@
+function add() {
+   return `{add}` 
+}
+add()
+
+function subtract() {
+   return `{subtract}`
+}
+subtract()
+
+function multiply() {
+   return `{multiply}`
+}
+multiply()
+
+function divide() {
+  return `{divide}` 
+}
+divide() 
+
+var a=80
+var b =60
+
+function add(a,b){
+   return (a+b);
+}
+function subtract(a,b) {
+  return (a-b);
+}
+function multiply(a, b){
+   return (a* b);
+}
+function divide(a, b) {
+   return (a/b);
+}
+function increment(n) {
+   return (++n);
+}
+function decrement(n) {
+   return(--n);
+}
+const makeInt=(n)=>{
+   return parseInt(n, 10);
+}
+const  preserveDecimal= (n)=>{
+   return parseFloat(n);
+}
+
